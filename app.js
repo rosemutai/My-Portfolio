c389a5a8ae0aae2e52c264556cca54954c6c20ba
rosemutai <chepngetichrose2030@gmail.com>
2023-03-06 18:29:42 +0300

    Fix linter errors

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-let navLinks = document.getElementById('menu-links')
-let hamburger = document.getElementById('hamburger')
-let iconCancel = document.getElementById('iconCancel')
-let maincontent = document.getElementById('maincontent')
+const navLinks = document.getElementById('menu-links');
+const hamburger = document.getElementById('hamburger');
+const iconCancel = document.getElementById('iconCancel');
+const maincontent = document.getElementById('maincontent');
 
-function OpenNavBarMenu (){
-    navLinks.style.visibility = "visible"
-    hamburger.style.display = "none"
-    maincontent.classList.add("blur")
+function OpenNavBarMenu() {
+  navLinks.style.visibility = 'visible';
+  hamburger.style.display = 'none';
+  maincontent.classList.add('blur');
 }
 
-function closeNavBarMenu (){
-    navLinks.style.visibility = "hidden"
-    hamburger.style.display = "block"
-    hamburger.style.position = "absolute"
-    maincontent.classList.remove("blur")
+function closeNavBarMenu() {
+  navLinks.style.visibility = 'hidden';
+  hamburger.style.display = 'block';
+  hamburger.style.position = 'absolute';
+  maincontent.classList.remove('blur');
 }
