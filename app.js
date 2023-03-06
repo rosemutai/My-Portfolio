55cff1d89826bbcb38194d0271316813c07cf00d
HASNAT KHAN <Hasnatking1947@gmail.com>
2023-03-06 17:29:04 +0500

    new changes

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
-let hamburger = document.getElementById('hamburger')
-let navLinks = document.getElementById('menu-links')
+
+function menu (){
+    let navLinks = document.getElementById('menu-links')
+    let nav = document.getElementById('nav')
+    let iconCancle = document.getElementById('iconCancle')
+    nav.style.display = "none"
+    navLinks.style.visibility = "visible"
+    iconCancle.style.display = "flex"
+}
+
+function cancleIcon (){
+    let navLinks = document.getElementById('navLinks')
+    navLinks.style.visibility = "hidden"
+    
+    console.log(navLinks);
+}
