24c7fb277bdc098e58547470c667cadc77b2b03c
rosemutai <chepngetichrose2030@gmail.com>
2023-03-06 18:43:42 +0300

    Fix linter errors

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 const navLinks = document.getElementById('menu-links');
 const hamburger = document.getElementById('hamburger');
-const iconCancel = document.getElementById('iconCancel');
 const maincontent = document.getElementById('maincontent');
 
-function OpenNavBarMenu() {
+export default function OpenNavBarMenu() {
   navLinks.style.visibility = 'visible';
   hamburger.style.display = 'none';
   maincontent.classList.add('blur');
 }
 
-function closeNavBarMenu() {
+export default function closeNavBarMenu() {
   navLinks.style.visibility = 'hidden';
   hamburger.style.display = 'block';
   hamburger.style.position = 'absolute';
