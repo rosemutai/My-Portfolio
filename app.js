7f232d866475252b59f587b2f74007948ca17012
HASNAT KHAN <Hasnatking1947@gmail.com>
2023-03-07 14:12:58 +0500

    added qurey sellector

--- app.js
+++ app.js
@@ -2,15 +2,17 @@ const navLinks = document.getElementById('menu-links');
 const hamburger = document.getElementById('hamburger');
 const maincontent = document.getElementById('maincontent');
 
-export function OpenNavBarMenu() {
+hamburger.addEventListener('click' , (()=>{
   navLinks.style.visibility = 'visible';
   hamburger.style.display = 'none';
   maincontent.classList.add('blur');
-}
+}))
 
-export function closeNavBarMenu() {
+const iconCancel = document.querySelector('#iconCancel')
+
+iconCancel.addEventListener('click' , (()=>{
   navLinks.style.visibility = 'hidden';
   hamburger.style.display = 'block';
   hamburger.style.position = 'absolute';
   maincontent.classList.remove('blur');
-}
+}))
