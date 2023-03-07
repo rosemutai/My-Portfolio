c2b9fc3e267ad36a73519ebfdff8deecb52b8098
rosemutai <chepngetichrose2030@gmail.com>
2023-03-07 15:22:07 +0300

    Fix linter errors

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const navLinks = document.getElementById('menu-links');
 const hamburger = document.getElementById('hamburger');
 const maincontent = document.getElementById('maincontent');
 const iconCancel = document.querySelector('#iconCancel');
-const openModalBtn = document.getElementById('view-project-btn')
 const menuLinks = document.querySelectorAll('.menu-anchor');
 
 hamburger.addEventListener('click', (() => {
@@ -18,13 +17,13 @@ iconCancel.addEventListener('click', (() => {
   maincontent.classList.remove('blur');
 }));
 
-for (let i=0; i < menuLinks.length; i+=1) {
-    menuLinks[i].addEventListener('click', () => {
-    maincontent.classList.remove('blur');
-    navLinks.style.visibility = 'hidden';
-    hamburger.classList.remove('blur');
-    hamburger.style.display = 'block';
-    hamburger.style.position = 'absolute';
-    hamburger.style.display = 'flex';
+for (let i = 0; i < menuLinks.length; i += 1) {
+  menuLinks[i].addEventListener('click', () => {
+  maincontent.classList.remove('blur');
+  navLinks.style.visibility = 'hidden';
+  hamburger.classList.remove('blur');
+  hamburger.style.display = 'block';
+  hamburger.style.position = 'absolute';
+  hamburger.style.display = 'flex';
   })
-}
+};
