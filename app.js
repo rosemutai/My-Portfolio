cea8747089ab0c44cf119996240053c24ebb0496
rosemutai <chepngetichrose2030@gmail.com>
2023-03-07 15:18:10 +0300

    Remove blur effect on links' click

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const navLinks = document.getElementById('menu-links');
 const hamburger = document.getElementById('hamburger');
 const maincontent = document.getElementById('maincontent');
 const iconCancel = document.querySelector('#iconCancel');
+const openModalBtn = document.getElementById('view-project-btn')
+const menuLinks = document.querySelectorAll('.menu-anchor');
 
 hamburger.addEventListener('click', (() => {
   navLinks.style.visibility = 'visible';
@@ -15,3 +17,14 @@ iconCancel.addEventListener('click', (() => {
   hamburger.style.position = 'absolute';
   maincontent.classList.remove('blur');
 }));
+
+for (let i=0; i < menuLinks.length; i+=1) {
+    menuLinks[i].addEventListener('click', () => {
+    maincontent.classList.remove('blur');
+    navLinks.style.visibility = 'hidden';
+    hamburger.classList.remove('blur');
+    hamburger.style.display = 'block';
+    hamburger.style.position = 'absolute';
+    hamburger.style.display = 'flex';
+  })
+}
