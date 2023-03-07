af05865aaaf746365b43fc786f3591c36adec310
rosemutai <chepngetichrose2030@gmail.com>
2023-03-07 12:17:56 +0300

    Fix linter errors

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const navLinks = document.getElementById('menu-links');
 const hamburger = document.getElementById('hamburger');
 const maincontent = document.getElementById('maincontent');
+const iconCancel = document.querySelector('#iconCancel');
 
 hamburger.addEventListener('click' , (()=>{
   navLinks.style.visibility = 'visible';
   hamburger.style.display = 'none';
   maincontent.classList.add('blur');
 }))
-
-const iconCancel = document.querySelector('#iconCancel')
 
 iconCancel.addEventListener('click' , (()=>{
   navLinks.style.visibility = 'hidden';
