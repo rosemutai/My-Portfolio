45f27442c7f1c0fbfd02a37d1442ddabe7840874
rosemutai <chepngetichrose2030@gmail.com>
2023-03-08 15:53:57 +0300

    Add mobile and desktop project details modal

--- app.js
+++ app.js
@@ -3,6 +3,87 @@ const hamburger = document.getElementById('hamburger');
 const maincontent = document.getElementById('maincontent');
 const iconCancel = document.querySelector('#iconCancel');
 const menuLinks = document.querySelectorAll('.menu-anchor');
+const closeModalButton = document.getElementById('closeModal')
+const modal = document.getElementById('details-modal')
+const seeModalButton = document.getElementsByClassName('open-modal-btn')
+const header = document.getElementsByClassName('header');
+const projectCards = document.getElementsByClassName('card');
+const projectsSection = document.getElementById('portfolio')
+const cardImageSection = document.getElementById('cardImg')
+let cardImage = document.querySelector('.project-image img')
+let cardProjectTitle = document.getElementsByClassName('project-title')
+const jobCompany = document.getElementsByClassName('canopy')
+const role = document.getElementsByClassName('career-role')
+const year = document.getElementsByClassName('year')
+const cardDescription = document.getElementsByClassName('modal-description')
+const modalImage = document.getElementById('modalImage')
+
+const projects = [
+  {
+    name: "Tonic",
+    experience: {
+      company: "canopy",
+      role: "Back End Dev",
+      year: 2015
+    },
+    description: "A daily selection of privately personalized reads no accounts or sign-ups required.", 
+    featuredImage: "./images/SnapshootPortfolio.svg",
+    technologies: [
+      "html", "css", "javascript"
+    ], 
+    liveVersionLink: "https://github.com/rosemutai/My-Portfolio", 
+    sourceCodeLink: "https://github.com/rosemutai/My-Portfolio/"
+  },
+
+  {
+    name: "Tonic",
+    experience: {
+      company: "canopy",
+      role: "Back End Dev",
+      year: 2015
+    },
+    description: "A daily selection of privately personalized reads no accounts or sign-ups required.", 
+    featuredImage: "./images/SnapshootPortfolio.svg",
+    technologies: [
+      "html", "css", "javascript"
+    ], 
+    liveVersionLink: "https://github.com/rosemutai/My-Portfolio", 
+    sourceCodeLink: "https://github.com/rosemutai/My-Portfolio/"
+  },
+
+  {
+    name: "Tonic",
+    experience: {
+      company: "canopy",
+      role: "Back End Dev",
+      year: 2015
+    },
+    description: "A daily selection of privately personalized reads no accounts or sign-ups required.", 
+    featuredImage: "./images/SnapshootPortfolio.svg",
+    technologies: [
+      "html", "css", "javascript"
+    ], 
+    liveVersionLink: "https://github.com/rosemutai/My-Portfolio", 
+    sourceCodeLink: "https://github.com/rosemutai/My-Portfolio/"
+  },
+
+  {
+    name: "Tonic",
+    experience: {
+      company: "canopy",
+      role: "Back End Dev",
+      year: 2015
+    },
+    description: "A daily selection of privately personalized reads no accounts or sign-ups required.", 
+    featuredImage: "./images/SnapshootPortfolio.svg",
+    technologies: [
+      "html", "css", "javascript"
+    ], 
+    liveVersionLink: "https://github.com/rosemutai/My-Portfolio", 
+    sourceCodeLink: "https://github.com/rosemutai/My-Portfolio/"
+  },
+]
+
 
 hamburger.addEventListener('click', (() => {
   navLinks.style.visibility = 'visible';
@@ -27,3 +108,61 @@ for (let i = 0; i < menuLinks.length; i += 1) {
     hamburger.style.display = 'flex';
   });
 }
+
+const addBlurEffect = () => {
+  maincontent.classList.toggle('modal-blur');
+  header[0].classList.toggle('modal-blur');
+}
+
+const toggleDetailsModal = () => {
+  modal.classList.toggle('visible')
+  addBlurEffect()
+}
+
+const modalTitle = document.getElementById('modal-title')
+
+const htmlBtns = document.querySelectorAll('.html-btn')
+for(let i = 0;i < htmlBtns.length; i += 1){
+  htmlBtns.innerHTML[i] = storeData[0].technologies[0]
+}
+
+const cssBtns = document.querySelectorAll('.css-btn')
+for(let i=0; i < cssBtns.length; i += 1) {
+  cssBtns.innerHTML[i] = storeData[0].technologies[1]
+}
+
+const jsBtns = document.querySelectorAll('.js-btn')
+for(let i=0; i < jsBtns.length;i+=1) {
+  jsBtns.innerHTML[i] = storeData[0].technologies[2]
+}
+
+const htmlSkill = document.getElementById('htmlSkill')
+
+function viewProject1 () {
+    toggleDetailsModal()
+    modalImage.src = projects[0].featuredImage
+    modalTitle.innerText = projects[0].name
+    cardDescription.innerText = projects[0].description  
+}
+
+function viewProject2 () {
+    toggleDetailsModal()
+    modalImage.src = projects[1].featuredImage
+    modalTitle.innerText = projects[1].name
+}
+
+function viewProject3 () {
+    toggleDetailsModal()
+    modalImage.src = projects[2].featuredImage
+    modalTitle.innerText = projects[2].name
+}
+
+function viewProject4 () {
+    toggleDetailsModal()
+    modalImage.src = projects[3].featuredImage
+    modalTitle.innerText = projects[3].name
+}
+
+
+
+closeModalButton.addEventListener('click', toggleDetailsModal)
